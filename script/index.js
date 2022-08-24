c4ce5f5f35de3dac7372bc25a7b83ad7faeaf162
panosjapan7 <panosjapan@gmail.com>
2022-08-24 14:59:06 +0200

    resolves some TypeScript errors

--- script/index.js
+++ script/index.js
@@ -2,23 +2,29 @@
 var input = document.querySelector("input");
 var form = document.querySelector("form");
 var errorMessage = document.querySelector(".error-message");
-
-// Runs validateEmail() when form is submitted
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    
-    var inputValue = input.value; // Saves input value
-
-    if (validateEmail(inputValue)) {
-        errorMessage.style.display = "none"; // Hides error <p> element
-        input.classList.remove("error"); // Makes input border blue
-    }
-    else {
-        errorMessage.style.display = "block"; // Shows error <p> element
-        input.classList.add("error"); // Makes input border red
-    }
-});
+// Runs validateInputs when form is submitted
+if (form != null) {
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        var inputValue = input.value; // Saves input value
+        if (validateEmail(inputValue)) {
+            console.log("valid email");
+            if (errorMessage != null) {
+                errorMessage.style.display = "none";
+            }
+            input.classList.remove("error");
+        }
+        else {
+            console.log("invalid email");
+            if (errorMessage != null) {
+                errorMessage.style.display = "block";
+            }
+            input.classList.add("error");
+        }
+    });
+}
 function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+// export {}; // 👈️ make file ES Module
--- script/index.ts
+++ script/index.ts
@@ -1,30 +1,37 @@
 
 // Selects Needed Form Elements in DOM
 let input = (<HTMLInputElement>document.querySelector("input"));
-let form = document.querySelector("form") as HTMLElement;
-let errorMessage = document.querySelector(".error-message");
+let form = document.querySelector("form") as HTMLElement | null;
+let errorMessage = document.querySelector(".error-message") as HTMLElement | null;
 
-// HTMLFormElement
 
 // Runs validateInputs when form is submitted
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let inputValue = input.value; // Saves input value
+if (form !=null) {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let inputValue = input.value; // Saves input value
+        
+        if(validateEmail(inputValue)) {
+            console.log("valid email");
+            if(errorMessage != null) {
+                errorMessage.style.display = "none";
+            }
+            input.classList.remove("error");
+        }
+        else {
+            console.log("invalid email");
+            if(errorMessage != null) { 
+                errorMessage.style.display = "block";
+            }
+            input.classList.add("error");
+        }
     
-    if(validateEmail(inputValue)) {
-        console.log("valid email");
-        errorMessage.style.display = "none";
-        input.classList.remove("error");
-    }
-    else {
-        console.log("invalid email");
-        errorMessage.style.display = "block";
-        input.classList.add("error");
-    }
+    })
+}
 
-})
-
-function validateEmail(email) {
+function validateEmail(email: String) {
     let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+// export {}; // 👈️ make file ES Module
